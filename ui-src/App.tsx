155bd67e74dfa50e07ba309224b39b31fc83f670
Doko Zero <17993323+dokozero@users.noreply.github.com>
2023-01-15 11:23:26 +0100

    Optimized canvas to use only one

--- ui-src/App.tsx
+++ ui-src/App.tsx
@@ -37,8 +37,7 @@ function clamp(x) {
 export function App() { 
 
   // We could use one canvas element but better no to avoid flickering when user change color model 
-  const canvasColorPickerOkHSL = useRef(null);
-  const canvasColorPickerOkHSV = useRef(null);
+  const canvasColorPicker = useRef(null);
   const canvasHueSlider = useRef(null);
   const manipulatorColorPicker = useRef(null);
   const manipulatorHueSlider = useRef(null);
@@ -76,13 +75,13 @@ export function App() {
 
   function display_results_okhsl(results) {
     // console.log("display_results_okhsl");
-    let ctx = canvasColorPickerOkHSL.current.getContext('2d');
+    let ctx = canvasColorPicker.current.getContext('2d');
     ctx.putImageData(results["okhsl_sl"], 0, 0);
   }
 
   function display_results_okhsv(results) {
     // console.log("display_results_okhsv");
-    let ctx = canvasColorPickerOkHSV.current.getContext('2d');
+    let ctx = canvasColorPicker.current.getContext('2d');
     ctx.putImageData(results["okhsv_sv"], 0, 0);
   }
 
@@ -117,16 +116,12 @@ export function App() {
 
   function renderCanvas() {
     setTimeout(function(){
-      if (colorModel.value == "okhsl" || init) {
+      if (colorModel.value == "okhsl") {
         display_results_okhsl(render_okhsl(rgbValues.r.value, rgbValues.g.value, rgbValues.b.value));
       }
 
-      if (colorModel.value == "okhsv" || init) {
+      if (colorModel.value == "okhsv") {
         display_results_okhsv(render(rgbValues.r.value, rgbValues.g.value, rgbValues.b.value));
-      }
-
-      if (init) {
-        init = false;
       }
 
     }, 5);
@@ -191,32 +186,33 @@ export function App() {
   function setup_handlers() {
     // console.log("setup_hsl_handler()");
 
-    setup_handler(canvasColorPickerOkHSL.current, function(x, y) {
-      let h = okhslValues.hue.value / 360;
-      let new_s = clamp(x/picker_size);
-      let new_l = clamp(1 - y/picker_size);
-
-      let rgb = okhsl_to_srgb(h, new_s, new_l);
-
-      rgbValues.r.value = rgb[0];
-      rgbValues.g.value = rgb[1];
-      rgbValues.b.value = rgb[2];
-
-      computeOkhslValues();
-    });
-
-    setup_handler(canvasColorPickerOkHSV.current, function(x, y) {
-      let h = okhsvValues.hue.value / 360;
-      let new_s = clamp(x/picker_size);
-      let new_v = clamp(1 - y/picker_size);
-
-      let rgb = okhsv_to_srgb(h, new_s, new_v);
-
-      rgbValues.r.value = rgb[0];
-      rgbValues.g.value = rgb[1];
-      rgbValues.b.value = rgb[2];
-
-      computeOkhsvValues();
+    setup_handler(canvasColorPicker.current, function(x, y) {
+      if (colorModel.value == "okhsl") {
+        let h = okhslValues.hue.value / 360;
+        let new_s = clamp(x/picker_size);
+        let new_l = clamp(1 - y/picker_size);
+  
+        let rgb = okhsl_to_srgb(h, new_s, new_l);
+  
+        rgbValues.r.value = rgb[0];
+        rgbValues.g.value = rgb[1];
+        rgbValues.b.value = rgb[2];
+  
+        computeOkhslValues();
+      }
+      else if (colorModel.value == "okhsv") {
+        let h = okhsvValues.hue.value / 360;
+        let new_s = clamp(x/picker_size);
+        let new_v = clamp(1 - y/picker_size);
+  
+        let rgb = okhsv_to_srgb(h, new_s, new_v);
+  
+        rgbValues.r.value = rgb[0];
+        rgbValues.g.value = rgb[1];
+        rgbValues.b.value = rgb[2];
+  
+        computeOkhsvValues();
+      }
     });
 
     setup_handler(canvasHueSlider.current, function(x, y) {
@@ -344,6 +340,7 @@ export function App() {
       let results = render_static();
       display_hue_slider(results);
       setup_handlers();
+      init = false;
     }
 
     updateManipulators();
@@ -362,8 +359,7 @@ export function App() {
 
       <div class="colorpicker">
 
-        <canvas ref={canvasColorPickerOkHSL} class={"colorpicker__canvas" + (colorModel.value == "okhsl" ? "" : " u-display-none")} id="okhsl_sl_canvas" width="257" height="257"></canvas>
-        <canvas ref={canvasColorPickerOkHSV} class={"colorpicker__canvas" + (colorModel.value == "okhsv" ? "" : " u-display-none")} id="okhsv_sv_canvas" width="257" height="257"></canvas>
+        <canvas ref={canvasColorPicker} class="colorpicker__canvas" id="okhsl_sl_canvas" width="257" height="257"></canvas>
 
         <svg class="colorpicker__handler" width="257" height="257">
           <g transform="translate(0,0)">
