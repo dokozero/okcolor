8ce84d8a84d2ee59470ac228e35c0b762e4ecbf2
Doko Zero <17993323+dokozero@users.noreply.github.com>
2023-11-03 22:47:57 +0100

    Fixed color picker manipulator out of lockContrast line When contrast is between -1 and 1

--- src/ui/stores/contrasts/lockContrast.ts
+++ src/ui/stores/contrasts/lockContrast.ts
@@ -3,6 +3,10 @@ import { logger } from '@nanostores/logger'
 import { consoleLogInfos } from '../../../constants'
 import { SyncLockContrastData } from '../../../types'
 import sendMessageToBackend from '../../helpers/sendMessageToBackend'
+import { $contrast } from './contrast'
+import getNewXandYFromContrast from '../../helpers/contrasts/getNewXandYFromContrast'
+import { $colorHxya, setColorHxyaWithSideEffects } from '../colors/colorHxya'
+import { $lockRelativeChroma } from '../colors/lockRelativeChroma'
 
 export const $lockContrast = atom(false)
 
@@ -28,6 +32,21 @@ export const setLockContrastWithSideEffects = action($lockContrast, 'setLockCont
       data: {
         lockContrast: newLockContrast
       }
+    })
+  }
+
+  // if lockConstrat is true, and the contrast is equal to 0 (in APCA it can be 0 but not -1 or 1) or -1 or 1 in WCAG, we run this code to adjust the Y (and X). That's because, for example in APCA, we frequently have multiple Y values for a contrast of 0, so without this correction, when setting lockChroma to true, the manipulator in the color picker could be out of the lockContrast line.
+  if (newLockContrast && $contrast.get() >= -1 && $contrast.get() <= 1) {
+    const newXy = getNewXandYFromContrast({
+      currentH: $colorHxya.get().h,
+      currentX: $colorHxya.get().x,
+      targetContrast: 0,
+      lockRelativeChroma: $lockRelativeChroma.get()
+    })
+
+    setColorHxyaWithSideEffects({
+      newColorHxya: newXy,
+      syncContrast: false
     })
   }
 })
