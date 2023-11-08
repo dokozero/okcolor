a36364651880ea44522c3cc104956b3e3b7f3525
Doko Zero <17993323+dokozero@users.noreply.github.com>
2023-11-08 17:59:07 +0100

    Fixed bad formats option in vite-backend config

--- vite-backend.config.ts
+++ vite-backend.config.ts
@@ -9,7 +9,7 @@ export default defineConfig({
       entry: resolve(__dirname, './src/backend/main.ts'),
       // the proper extensions will be added.
       fileName: 'backend',
-      formats: ['cjs']
+      formats: ['es']
     },
     outDir: resolve(__dirname, 'dist'),
     emptyOutDir: false
