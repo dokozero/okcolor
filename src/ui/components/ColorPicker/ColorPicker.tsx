fc6031719dd51988a51f3c73c67815fd4bf757ab
Doko Zero <17993323+dokozero@users.noreply.github.com>
2025-03-29 12:15:42 +0100

    Added feature to change xy values with arrow keys on color picker

--- src/ui/components/ColorPicker/ColorPicker.tsx
+++ src/ui/components/ColorPicker/ColorPicker.tsx
@@ -36,6 +36,8 @@ import { $selectionId } from '../../stores/selectionId/selectionId'
 import { $isTransitionRunning, $oklchRenderMode, setIsTransitionRunning } from '../../stores/oklchRenderMode/oklchRenderMode'
 import convertRelativeChromaToAbsolute from '../../helpers/colors/convertRelativeChromaToAbsolute/convertRelativeChromaToAbsolute'
 import getLinearMappedValue from '../../helpers/getLinearMappedValue/getLinearMappedValue'
+import handleKeyDown from './helpers/handleKeyDown/handleKeyDown'
+import handleWheel from './helpers/handleWheel/handleWheel'
 
 // We use these var to measure speeds of color picker rendering (see in constants file to activate it).
 let colorPickerStrokesRenderingStart: number
@@ -576,84 +578,26 @@ export default function ColorPicker() {
       setMouseEventCallback(handleNewManipulatorPosition)
     })
 
-    document.addEventListener('keyup', () => {
-      currentConstrainedMove = false
-      mainMouseMovement = null
+    // Change x or y values from key press.
+    colorPicker.current!.addEventListener('keydown', (event) => {
+      if ($isTransitionRunning.get()) return
+      if (!['ArrowUp', 'ArrowRight', 'ArrowDown', 'ArrowLeft'].includes(event.key)) return
+
+      handleKeyDown(event.key as 'ArrowUp' | 'ArrowDown' | 'ArrowLeft' | 'ArrowRight')
     })
 
-    // Detect wheel scroll on color picker to change hue.
+    // Change hue value on vertical wheel.scroll
     colorPicker.current!.addEventListener('wheel', (event) => {
       if ($isTransitionRunning.get()) return
 
-      let currentH = $colorHxya.get().h
-      let valueToAdd = event.deltaY / 8
-
-      // If deltaY is not equal to 0 or -0 when shift is pressed, it means that user has a trackpad.
-      // That's because with a mouse and whift pressed, system whill trigger horizontal scrolling, thus keeping deltaY to 0 and update deltaX instead, but not with trackpads like Apple's one.
-      // In that case, we avoid constraining movement to value of 5 as the trackpad is not precise enough.
-      if (event.shiftKey && event.deltaY === 0) {
-        // Round the value to the nearest multiple of 5 value. E.g. if hue is 134, we round it to 135.
-        // This is usefull to have value multiple of 5 when user press shift no matter the initial hue value.
-        currentH = Math.round(currentH / 5) * 5
-
-        if (event.deltaX > 0) {
-          valueToAdd = 5
-        } else {
-          valueToAdd = -5
-        }
-      }
-
-      let newH = round(currentH + valueToAdd, getColorHxyDecimals().h)
-
-      // To allows for an infinite scroll loop.
-      if (newH < 0) {
-        newH = 360
-      } else if (newH > 360) {
-        newH = 0
-      }
-
-      if ($oklchRenderMode.get() === 'triangle') {
-        setColorHxyaWithSideEffects({
-          newColorHxya: {
-            h: newH
-          }
-        })
-      } else if ($oklchRenderMode.get() === 'square') {
-        const newXValue = convertRelativeChromaToAbsolute({
-          h: newH,
-          y: $colorHxya.get().y
-        })
-
-        setColorHxyaWithSideEffects({
-          newColorHxya: {
-            x: newXValue,
-            h: newH
-          },
-          sideEffects: {
-            syncRelativeChroma: false
-          }
-        })
-      }
-    })
-
-    colorPicker.current!.addEventListener('mousemove', (event) => {
-      const deltaX = Math.abs(event.clientX - lastMouseX)
-      const deltaY = Math.abs(event.clientY - lastMouseY)
-
-      if (Math.abs(deltaX - deltaY) && !currentConstrainedMove) {
-        if (deltaX > deltaY) mainMouseMovement = 'horizontal'
-        else mainMouseMovement = 'vertical'
-      }
-
-      lastMouseX = event.clientX
-      lastMouseY = event.clientY
+      handleWheel(event)
     })
 
     isMounted.current = true
   }, [])
 
   return (
-    <div ref={colorPicker} className="c-color-picker" style={{ width: `${PICKER_SIZE}px`, height: `${PICKER_SIZE}px` }}>
+    <div ref={colorPicker} tabIndex={0} className="c-color-picker" style={{ width: `${PICKER_SIZE}px`, height: `${PICKER_SIZE}px` }}>
       <div className="c-color-picker__message-wrapper">
         <p className="c-color-picker__message-text">{uiMessage.message}</p>
       </div>
---
+++ src/ui/components/ColorPicker/helpers/handleKeyDown/handleKeyDown.ts
@@ -0,0 +1,64 @@
+import { $colorHxya, setColorHxyaWithSideEffects } from '../../../../stores/colors/colorHxya/colorHxya'
+import { $lockRelativeChroma } from '../../../../stores/colors/lockRelativeChroma/lockRelativeChroma'
+import { $relativeChroma, setRelativeChromaWithSideEffects } from '../../../../stores/colors/relativeChroma/relativeChroma'
+import { $lockContrast } from '../../../../stores/contrasts/lockContrast/lockContrast'
+import { $currentKeysPressed } from '../../../../stores/currentKeysPressed/currentKeysPressed'
+import { $oklchRenderMode } from '../../../../stores/oklchRenderMode/oklchRenderMode'
+import getStepUpdateValue from '../../../ColorValueInputs/helpers/getStepUpdateValue/getStepUpdateValue'
+
+export default function handleKeyDown(eventKey: 'ArrowUp' | 'ArrowDown' | 'ArrowLeft' | 'ArrowRight') {
+  if ($lockRelativeChroma.get() && (eventKey === 'ArrowLeft' || eventKey === 'ArrowRight')) return
+
+  if ($lockContrast.get() && (eventKey === 'ArrowUp' || eventKey === 'ArrowDown')) return
+
+  let newValue = 0
+  let stepUpdateValue = 0
+
+  const axis = eventKey === 'ArrowUp' || eventKey === 'ArrowDown' ? 'y' : 'x'
+
+  if (axis === 'y') {
+    newValue = $colorHxya.get().y
+
+    if ($currentKeysPressed.get().includes('shift')) {
+      newValue = Math.round(newValue / 5) * 5
+    }
+
+    stepUpdateValue = $currentKeysPressed.get().includes('shift') ? 5 : 1
+  } else if (axis === 'x') {
+    if ($oklchRenderMode.get() === 'triangle') {
+      newValue = $colorHxya.get().x
+      stepUpdateValue = getStepUpdateValue('x')
+    } else {
+      newValue = $relativeChroma.get()
+
+      if ($currentKeysPressed.get().includes('shift')) {
+        newValue = Math.round(newValue / 5) * 5
+      }
+
+      stepUpdateValue = $currentKeysPressed.get().includes('shift') ? 5 : 1
+    }
+  }
+
+  if (eventKey === 'ArrowUp' || eventKey === 'ArrowRight') {
+    newValue += stepUpdateValue
+  } else if (eventKey === 'ArrowDown' || eventKey === 'ArrowLeft') {
+    newValue -= stepUpdateValue
+  }
+
+  if ($oklchRenderMode.get() === 'square' && axis === 'x') {
+    setRelativeChromaWithSideEffects({
+      newRelativeChroma: newValue
+    })
+  } else {
+    let localLockRelativeChroma = $lockRelativeChroma.get()
+
+    if (axis === 'y' && $oklchRenderMode.get() === 'square') {
+      localLockRelativeChroma = true
+    }
+
+    setColorHxyaWithSideEffects({
+      newColorHxya: axis === 'y' ? { y: newValue } : { x: newValue },
+      lockRelativeChroma: localLockRelativeChroma
+    })
+  }
+}
